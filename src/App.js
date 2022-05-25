b4ad9bb69ff38afad6b5d7d91b4433e932b1791c
Hudson Menezes <hudson.bm23@gmail.com>
2022-05-25 17:06:14 -0300

    add filter ( year )

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useEffect, useState } from 'react'
 function App() {
   const [cars, setCars] = useState([])
   const [loading, setLoading] = useState(false) // valor inicial "false" para a tela de loading
+  const [year, setYear] = useState('')
 
   const getCars = () => {
     setCars([]) // limpa a lista setCars e carrega novamente
     setLoading(true) // quando iniciar a requisição, vai gerar tela de loading
-    fetch('https://api.b7web.com.br/carros/api/carros')
+    fetch(`https://api.b7web.com.br/carros/api/carros?ano=${year}`)
       .then(function (result) {
         // recebe o 'result' e transforma em json através de .json()
         return result.json()
@@ -24,18 +25,42 @@ function App() {
       })
   }
 
+  const handleYearChange = e => {
+    setYear(e.target.value) // pega o valor das opções em <option>
+  }
+
   useEffect(() => {
     // ao atualizar a tela, faz a requisição
     getCars()
-  }, [])
+  }, [year])
 
   return (
     <div>
       <h1>Lista de Carros</h1>
+
+      <select onChange={handleYearChange}>
+        <option></option>
+        <option>2022</option>
+        <option>2021</option>
+        <option>2020</option>
+        <option>2019</option>
+        <option>2018</option>
+        <option>2017</option>
+        <option>2016</option>
+        <option>2015</option>
+        <option>2014</option>
+        <option>2013</option>
+        <option>2012</option>
+      </select>
+
       <button onClick={getCars}>Atualizar Lista!</button>
       <hr />
 
+      {/* Tela de Loading e Tela de Nenhum Carro encontrado */}
       {loading === true && <h2>Carregando as informações...</h2>}
+      {cars.length === 0 && loading === false && (
+        <h2>Nenhum Carro Encontrado.</h2>
+      )}
 
       {cars.map((item, index) => (
         <div key={index}>
