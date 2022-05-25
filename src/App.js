cb3f95e9a6bc260030bdafd74fe9d538985b26bd
Hudson Menezes <hudson.bm23@gmail.com>
2022-05-25 15:22:12 -0300

    first commit - fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [cars, setCars] = useState([])
+
+  const getCars = () => {
+    fetch('https://api.b7web.com.br/carros/api/carros')
+      .then(function (result) {
+        return result.json()
+      })
+      .then(function (json) {
+        if (json.error === '') {
+          setCars(json.cars)
+        } else {
+          alert(json.error)
+        }
+      })
+  }
+
+  useEffect(() => {
+    getCars()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Lista de Carros</h1>
+      <button onClick={getCars}>Atualizar Lista!</button>
+      <hr />
+      {cars.map((item, index) => (
+        <div key={index}>
+          <img src={item.photo} width="300px" />
+          <h3>
+            {item.brand} - {item.name}
+          </h3>
+          <p>
+            {item.year} - R${item.price}
+          </p>
+        </div>
+      ))}
+    </>
+  )
 }
 
-export default App;
+export default App
+
+// API: https://api.b7web.com.br/carros/api/carros
+
+// {cars.map((item, index) => (
+//   <div key={index}>
+//     <img src={item.photo} width="200px" />
+//     <h3>
+//       {item.brand} - {item.name}
+//     </h3>
+//     <p>
+//       {item.year} - R$ {item.price}
+//     </p>
+//   </div>
+// ))}
